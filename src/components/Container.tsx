da6970d6c02ebadaa97b308b2538f5bee7bdb2de
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-07 21:49:32 +0200

    done

---
+++ src/components/Container.tsx
@@ -0,0 +1,5 @@
+function Container({ children }: { children: React.ReactNode }) {
+  return <div className='container'>{children}</div>;
+}
+
+export default Container;
--- src/components/useHover.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * @hook useHover
- * @returns {JSX.Element}
- */
-function useHover() {
-  return <div className='w-20 aspect-square rounded-full bg-white'>Hover</div>;
-}
-
-export default useHover;
