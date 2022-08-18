bb26847f7bf1460cf33353d11114900c77b24123
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-08-18 15:35:05 +0200

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,13 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
+        <title>Alan | Home</title>
       </Head>
-      <main className={styles.container}>
-        <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
-        </h1>
-      </main>
+      
     </div>
   );
 };
